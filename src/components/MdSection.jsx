277f1cfa95439d6415f67ca18eb6c39489f8bdd0
yoon664 <yoonj0264@gmail.com>
2025-08-16 06:09:55 +0900

    .

--- src/components/MdSection.jsx
+++ src/components/MdSection.jsx
@@ -182,7 +182,7 @@ export default function MdSection() {
             className="relative w-[280px] h-[280px] flex items-center justify-center"
             style={{ 
               perspective: translateZ * 1 + "px",
-              transform: 'scale(1.8)'
+              transform: 'scale(3)'
             }}
           >
             <motion.ul
@@ -244,8 +244,32 @@ export default function MdSection() {
           </div>
         </div>
 
-        {/* VIEW ALL MD 버튼 */}
-        <div className="flex justify-center">
+        {/* VIEW ALL MD 버튼 - 데스크탑 */}
+        <div className="hidden md:flex justify-center">
+          <button className="relative group cursor-pointer transition-all duration-300">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="326"
+              height="96"
+              viewBox="0 0 326 96"
+              fill="none"
+            >
+              <path
+                d="M1 95V1H325V63.3654L298 95H1Z"
+                stroke="white"
+                strokeWidth="2"
+                fill="transparent"
+                className="group-hover:stroke-[#DF6D21] group-hover:fill-[#DF6D21] transition-all duration-300"
+              />
+            </svg>
+            <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-2xl font-bold tracking-wider whitespace-nowrap">
+              VIEW ALL MD
+            </span>
+          </button>
+        </div>
+
+        {/* VIEW ALL MD 버튼 - 모바일 */}
+        <div className="flex md:hidden justify-center">
           <button className="relative group cursor-pointer transition-all duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" width="200" height="59" viewBox="0 0 326 96" fill="none">
               <path d="M1 95V1H325V63.3654L298 95H1Z" 
